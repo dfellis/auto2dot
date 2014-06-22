2f250e2a500dd5d05e78ccccd70679d563652eeb
David Ellis <d.f.ellis@ieee.org>
2014-06-22 00:29:37 -0700

    The parser now works for the first test file I've run it against :)

--- index.js
+++ index.js
@@ -15,27 +15,98 @@ function getAst(js) {
     return esprima.parse(js);
 }
 
-function findAutoSubtree(ast) {
-    var body = ast.body;
-    var asyncIdentifier;
-    var autoObj;
-    while (!asyncIdentifier && !autoObj) {
-        // TODO: Write code to first find a variable declaration for the async object
-        // assuming that it comes first, then look for an `async.auto` call and
-        // return its first argument (the object that declares the dependency tree)
-        // TODO TODO: if we actually have a reference to the dependency tree
+function getAsyncIdentifier(ast) {
+    var result;
+    if (ast.type === 'Program') {
+        for (var i = 0; i < ast.body.length; i++) {
+            result = getAsyncIdentifier(ast.body[i]);
+            if (result) break;
+        }
+        return result;
+    } else if (ast.type === 'VariableDeclaration') {
+        for (var j = 0; j < ast.declarations.length; j++) {
+            result = getAsyncIdentifier(ast.declarations[j]);
+            if (result) break;
+        }
+        return result;
+    } else if (ast.type === 'VariableDeclarator') {
+        if (ast.init.type === 'CallExpression') {
+            if (ast.init.callee.type === 'Identifier' &&
+                ast.init.callee.name === 'require' &&
+                ast.init.arguments[0].type === 'Literal' &&
+                ast.init.arguments[0].value === 'async') {
+                return ast.id.name;
+            }
+        }
+        return undefined;
+    } else {
+        // TODO: Handle the rest of the tree
+        return undefined;
+    }
+}
+
+function findAutoSubtree(ast, asyncIdentifier) {
+    var result;
+    if (!asyncIdentifier) asyncIdentifier = getAsyncIdentifier(ast);
+    if (ast.type === 'Program') {
+        for (var i = 0; i < ast.body.length; i++) {
+            result = findAutoSubtree(ast.body[i], asyncIdentifier);
+            if (result) break;
+        }
+        return result;
+    } else if (ast.type === 'VariableDeclaration') {
+        for (var j = 0; j < ast.declarations.length; j++) {
+            result = findAutoSubtree(ast.declarations[j], asyncIdentifier);
+            if (result) break;
+        }
+        return result;
+    } else if (ast.type === 'VariableDeclarator') {
+        return ast.init ? findAutoSubtree(ast.init, asyncIdentifier) : undefined;
+    } else if (ast.type === 'FunctionDeclaration') {
+        for (var k = 0; k < ast.body.body.length; k++) {
+            result = findAutoSubtree(ast.body.body[k], asyncIdentifier);
+            if (result) break;
+        }
+        return result;
+    } else if (ast.type === 'ExpressionStatement') {
+        return findAutoSubtree(ast.expression, asyncIdentifier);
+    } else if (ast.type === 'CallExpression') {
+        if (ast.callee.type === 'MemberExpression' &&
+            ast.callee.object.name === asyncIdentifier &&
+            ast.callee.property.name === 'auto') {
+            return ast.arguments[0];
+        }
+        return undefined;
+    } else {
+        // TODO: Handle the rest of the tree
+        return undefined;
     }
 }
 
 function buildGraphVizDotString(subAst) {
-
+    if (subAst.type !== 'ObjectExpression') throw new Error('Does not support dynamic async.auto definitions');
+    var outString = 'digraph asyncAuto {\n';
+    for (var i = 0; i < subAst.properties.length; i++) {
+        var property = subAst.properties[i];
+        var name = property.key.name;
+        if (property.value.type === 'ArrayExpression') {
+            for (var j = 0; j < property.value.elements.length; j++) {
+                var element = property.value.elements[j];
+                if (element.type === 'Literal') {
+                    outString += '\t' + element.value + ' -> ' + name + ';\n';
+                }
+            }
+        }
+    }
+    outString += '}\n';
+    return outString;
 }
 
 function main() {
     commander
         .version(package.version)
-        .option('-s, --source', 'The Javascript source file to parse', String)
-        .option('-d, --destination', 'The GraphViz dot file to write', String)
+        .option('-s, --source <file>', 'The Javascript source file to parse', String)
+        .option('-d, --destination <file>', 'The GraphViz dot file to write', String)
         .parse(process.argv);
 
     if (!commander.source || !commander.destination) return commander.help();
@@ -47,11 +118,7 @@ function main() {
                 getAst(
                     loadFile(
                         commander.source
-                    )
-                )
-            )
-        )
-    ); // http://xkcd.com/297/
+    ))))); // http://xkcd.com/297/
 }
 
 if (module === require.main) {
