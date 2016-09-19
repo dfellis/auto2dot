b88a9ff2811ed0a5795b4c85ca140e50d163cd8b
Matt Blair <matt@mattblair.co>
2016-09-19 14:55:41 -0700

    ES6 update;  - add support for transpiled output from babel;  - move to eslint

--- index.js
+++ index.js
@@ -1,153 +1,161 @@
 #!/usr/bin/env node
-var commander = require('commander');
-var esprima = require('esprima');
-var fs = require('fs');
-var package = require('./package.json');
+const commander = require('commander');
+const esprima = require('esprima');
+const fs = require('fs');
+const package = require('./package.json');
 
 function loadFile(filename) {
-    return fs.readFileSync(filename, 'utf8');
+  return fs.readFileSync(filename, 'utf8');
 }
 
 function saveFile(filename, contents) {
-    return fs.writeFileSync(filename, contents, 'utf8');
+  return fs.writeFileSync(filename, contents, 'utf8');
 }
 
 function getAst(js) {
-    return esprima.parse(js);
+  return esprima.parse(js);
 }
 
 function getAsyncIdentifier(ast) {
-    var result;
-    if (ast.type === 'Program') {
-        for (var i = 0; i < ast.body.length; i++) {
-            result = getAsyncIdentifier(ast.body[i]);
-            if (result) break;
-        }
-        return result;
-    } else if (ast.type === 'VariableDeclaration') {
-        for (var j = 0; j < ast.declarations.length; j++) {
-            result = getAsyncIdentifier(ast.declarations[j]);
-            if (result) break;
-        }
-        return result;
-    } else if (ast.type === 'VariableDeclarator') {
-        if (ast.init.type === 'CallExpression') {
-            if (ast.init.callee.type === 'Identifier' &&
+  let result;
+  if (ast.type === 'Program') {
+    for (let i = 0; i < ast.body.length; i++) {
+      result = getAsyncIdentifier(ast.body[i]);
+      if (result) break;
+    }
+    return result;
+  } else if (ast.type === 'VariableDeclaration') {
+    for (let j = 0; j < ast.declarations.length; j++) {
+      result = getAsyncIdentifier(ast.declarations[j]);
+      if (result) break;
+    }
+    return result;
+  } else if (ast.type === 'VariableDeclarator') {
+    if (ast.init.type === 'CallExpression') {
+      if (ast.init.callee.type === 'Identifier' &&
                 ast.init.callee.name === 'require' &&
                 ast.init.arguments[0].type === 'Literal' &&
-                ast.init.arguments[0].value === 'async') {
-                return ast.id.name;
-            }
-        }
-        return undefined;
-    } else {
-        // TODO: Handle the rest of the tree
-        return undefined;
+                ast.init.arguments[0].value === 'lib/utils') {
+        return ast.id.name;
+      }
     }
+    return undefined;
+  }
+
+  // TODO: Handle the rest of the tree
+  return undefined;
 }
 
 function findAutoSubtree(ast, asyncIdentifier) {
-    var result;
-    if (!asyncIdentifier) asyncIdentifier = getAsyncIdentifier(ast);
-    if (ast.type === 'Program') {
-        for (var i = 0; i < ast.body.length; i++) {
-            result = findAutoSubtree(ast.body[i], asyncIdentifier);
-            if (result) break;
-        }
-        return result;
-    } else if (ast.type === 'VariableDeclaration') {
-        for (var j = 0; j < ast.declarations.length; j++) {
-            result = findAutoSubtree(ast.declarations[j], asyncIdentifier);
-            if (result) break;
-        }
-        return result;
-    } else if (ast.type === 'VariableDeclarator') {
-        return ast.init ? findAutoSubtree(ast.init, asyncIdentifier) : undefined;
-    } else if (ast.type === 'FunctionDeclaration') {
-        for (var k = 0; k < ast.body.body.length; k++) {
-            result = findAutoSubtree(ast.body.body[k], asyncIdentifier);
-            if (result) break;
-        }
-        return result;
-    } else if (ast.type === 'FunctionExpression') {
-        for (var l = 0; l < ast.body.body.length; l++) {
-            result = findAutoSubtree(ast.body.body[l], asyncIdentifier);
-            if (result) break;
-        }
-        return result;
-    } else if (ast.type === 'ExpressionStatement') {
-        return findAutoSubtree(ast.expression, asyncIdentifier);
-    } else if (ast.type === 'CallExpression') {
-        if (ast.callee.type === 'MemberExpression' &&
+  let result;
+  if (!asyncIdentifier) asyncIdentifier = getAsyncIdentifier(ast);
+
+  if (ast.type === 'Program') {
+    for (let i = 0; i < ast.body.length; i++) {
+      result = findAutoSubtree(ast.body[i], asyncIdentifier);
+      if (result) break;
+    }
+    return result;
+  } else if (ast.type === 'VariableDeclaration') {
+    for (let j = 0; j < ast.declarations.length; j++) {
+      result = findAutoSubtree(ast.declarations[j], asyncIdentifier);
+      if (result) break;
+    }
+    return result;
+  } else if (ast.type === 'VariableDeclarator') {
+    return ast.init ? findAutoSubtree(ast.init, asyncIdentifier) : undefined;
+  } else if (ast.type === 'FunctionDeclaration') {
+    for (let k = 0; k < ast.body.body.length; k++) {
+      result = findAutoSubtree(ast.body.body[k], asyncIdentifier);
+      if (result) break;
+    }
+    return result;
+  } else if (ast.type === 'FunctionExpression') {
+    for (let l = 0; l < ast.body.body.length; l++) {
+      result = findAutoSubtree(ast.body.body[l], asyncIdentifier);
+      if (result) break;
+    }
+    return result;
+  } else if (ast.type === 'ExpressionStatement') {
+    return findAutoSubtree(ast.expression, asyncIdentifier);
+  } else if (ast.type === 'CallExpression') {
+    if (ast.callee.type === 'SequenceExpression') {
+      const possibleAuto = ast.callee.expressions[1];
+
+      if (possibleAuto &&
+            possibleAuto.object.name === asyncIdentifier &&
+            possibleAuto.property.name === 'auto') {
+        return ast.arguments[0];
+      }
+    }
+
+    if (ast.callee.type === 'MemberExpression' &&
             ast.callee.object.name === asyncIdentifier &&
             ast.callee.property.name === 'auto') {
-            return ast.arguments[0];
-        }
-        return undefined;
-    } else if (ast.type === 'AssignmentExpression') {
-        return findAutoSubtree(ast.right, asyncIdentifier);
-    } else if (ast.type === 'BinaryExpression') {
-        result = findAutoSubtree(ast.left, asyncIdentifier);
-        if (result) return result;
-        result = findAutoSubtree(ast.right, asyncIdentifier);
-        return result;
-    } else if (ast.type === 'ReturnStatement') {
-        return findAutoSubtree(ast.argument, asyncIdentifier);
-    } else if (ast.type === 'Literal') {
-        return undefined;
-    } else {
-        // TODO: Handle the rest of the tree
-        return undefined;
+      return ast.arguments[0];
     }
+    return undefined;
+  } else if (ast.type === 'AssignmentExpression') {
+    return findAutoSubtree(ast.right, asyncIdentifier);
+  } else if (ast.type === 'BinaryExpression') {
+    result = findAutoSubtree(ast.left, asyncIdentifier);
+    if (result) return result;
+    result = findAutoSubtree(ast.right, asyncIdentifier);
+    return result;
+  } else if (ast.type === 'ReturnStatement') {
+    return findAutoSubtree(ast.argument, asyncIdentifier);
+  } else if (ast.type === 'Literal') {
+    return undefined;
+  }
+  // TODO: Handle the rest of the tree
+  return undefined;
 }
 
 function buildGraphVizDotString(subAst) {
-    if (subAst.type !== 'ObjectExpression') throw new Error('Does not support dynamic async.auto definitions');
-    var outString = 'digraph asyncAuto {\n';
-    for (var i = 0; i < subAst.properties.length; i++) {
-        var property = subAst.properties[i];
-        var name = property.key.name;
-        if (property.value.type === 'ArrayExpression') {
-            for (var j = 0; j < property.value.elements.length; j++) {
-                var element = property.value.elements[j];
-                if (element.type === 'Literal') {
-                    outString += '\t' + element.value + ' -> ' + name + ';\n';
-                }
-            }
+  if (subAst.type !== 'ObjectExpression') throw new Error('Does not support dynamic async.auto definitions');
+  let outString = 'digraph asyncAuto {\n';
+  for (let i = 0; i < subAst.properties.length; i++) {
+    const property = subAst.properties[i];
+    const name = property.key.name;
+    if (property.value.type === 'ArrayExpression') {
+      for (let j = 0; j < property.value.elements.length; j++) {
+        const element = property.value.elements[j];
+        if (element.type === 'Literal') {
+          outString += '\t' + element.value + ' -> ' + name + ';\n';
         }
+      }
     }
-    outString += '}\n';
-    return outString;
+  }
+  outString += '}\n';
+  return outString;
 }
 
 function main() {
-    commander
+  commander
         .version(package.version)
         .option('-s, --source <file>', 'The Javascript source file to parse', String)
         .option('-d, --destination <file>', 'The GraphViz dot file to write', String)
         .parse(process.argv);
 
-    if (!commander.source || !commander.destination) return commander.help();
+  if (!commander.source || !commander.destination) return commander.help();
 
-    saveFile(
-        commander.destination,
-        buildGraphVizDotString(
-            findAutoSubtree(
-                getAst(
-                    loadFile(
-                        commander.source
-    ))))); // http://xkcd.com/297/
+  const fileContent = buildGraphVizDotString(findAutoSubtree(getAst(loadFile(commander.source))));
+  // http://xkcd.com/297/
+
+  saveFile(commander.destination, fileContent);
+
+  return fileContent;
 }
 
 if (module === require.main) {
-    main();
+  main();
 } else {
-    module.exports = {
-        loadFile: loadFile,
-        saveFile: saveFile,
-        getAst: getAst,
-        getAsyncIdentifier: getAsyncIdentifier,
-        findAutoSubtree: findAutoSubtree,
-        buildGraphVizDotString: buildGraphVizDotString
-    };
+  module.exports = {
+    loadFile,
+    saveFile,
+    getAst,
+    getAsyncIdentifier,
+    findAutoSubtree,
+    buildGraphVizDotString
+  };
 }
--- tests/index.js
+++ tests/index.js
@@ -1,61 +1,61 @@
-var async2dot = require('..');
-var fs = require('fs');
+const async2dot = require('..');
+const fs = require('fs');
 
-var js;
+let js;
 
 exports.loadFile = function(test) {
-    test.expect(1);
-    js = async2dot.loadFile('./tests/examples/basic.js');
-    test.equal(js, fs.readFileSync('./tests/examples/basic.js', 'utf8'));
-    test.done();
+  test.expect(1);
+  js = async2dot.loadFile('./tests/examples/basic.js');
+  test.equal(js, fs.readFileSync('./tests/examples/basic.js', 'utf8'));
+  test.done();
 };
 
 exports.saveFile = function(test) {
-    test.expect(1);
-    async2dot.saveFile('./tests/examples/foo', 'bar');
-    test.equal('bar', fs.readFileSync('./tests/examples/foo', 'utf8'));
-    fs.unlinkSync('./tests/examples/foo');
-    test.done();
+  test.expect(1);
+  async2dot.saveFile('./tests/examples/foo', 'bar');
+  test.equal('bar', fs.readFileSync('./tests/examples/foo', 'utf8'));
+  fs.unlinkSync('./tests/examples/foo');
+  test.done();
 };
 
-var ast;
+let ast;
 
 exports.getAst = function(test) {
-    test.expect(1);
-    ast = async2dot.getAst(js);
-    test.equal(ast.type, 'Program');
-    test.done();
+  test.expect(1);
+  ast = async2dot.getAst(js);
+  test.equal(ast.type, 'Program');
+  test.done();
 };
 
-var asyncIdentifier;
+let asyncIdentifier;
 
 exports.getAsyncIdentifier = function(test) {
-    test.expect(1);
-    asyncIdentifier = async2dot.getAsyncIdentifier(ast);
-    test.equal(asyncIdentifier, 'async');
-    test.done();
+  test.expect(1);
+  asyncIdentifier = async2dot.getAsyncIdentifier(ast);
+  test.equal(asyncIdentifier, 'async');
+  test.done();
 };
 
-var subtree;
+let subtree;
 
 exports.findAutoSubtree = function(test) {
-    test.expect(1);
-    subtree = async2dot.findAutoSubtree(ast, asyncIdentifier);
-    test.equal(subtree.type, 'ObjectExpression');
-    test.done();
+  test.expect(1);
+  subtree = async2dot.findAutoSubtree(ast, asyncIdentifier);
+  test.equal(subtree.type, 'ObjectExpression');
+  test.done();
 };
 
 exports.buildGraphVizDotString = function(test) {
-    test.expect(1);
-    var dotString = async2dot.buildGraphVizDotString(subtree);
-    test.equal(typeof dotString, 'string');
-    test.done();
+  test.expect(1);
+  const dotString = async2dot.buildGraphVizDotString(subtree);
+  test.equal(typeof dotString, 'string');
+  test.done();
 };
 
 exports.fullStack = function(test) {
-    test.expect(1);
-    var result = fs.readFileSync('./tests/examples/basic.dot', 'utf8');
-    test.equal(
+  test.expect(1);
+  const result = fs.readFileSync('./tests/examples/basic.dot', 'utf8');
+  test.equal(
         result,
         async2dot.buildGraphVizDotString(
             async2dot.findAutoSubtree(
@@ -63,12 +63,12 @@ exports.fullStack = function(test) {
                     async2dot.loadFile(
                         './tests/examples/basic.js'
     )))));
-    test.done();
+  test.done();
 };
 
 exports.checkPathThatIgnoresNonAsyncRequires = function(test) {
-    test.expect(1);
-    test.equal(
+  test.expect(1);
+  test.equal(
         'ObjectExpression',
         async2dot.findAutoSubtree(
             async2dot.getAst(
@@ -76,18 +76,18 @@ exports.checkPathThatIgnoresNonAsyncRequires = function(test) {
                     './tests/examples/multirequire.js'
         ))).type
     );
-    test.done();
+  test.done();
 };
 
 exports.throwsWhenAutoIsDynamic = function(test) {
-    test.expect(1);
-    test.throws(function() {
-        async2dot.buildGraphVizDotString(
+  test.expect(1);
+  test.throws(function() {
+    async2dot.buildGraphVizDotString(
             async2dot.findAutoSubtree(
                 async2dot.getAst(
                     async2dot.loadFile(
                         './tests/examples/dynamicauto.js'
         ))));
-    });
-    test.done();
+  });
+  test.done();
 };
